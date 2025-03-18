64b3d0f6ec6530923b5260093e5c9754e08b5f9e
francescoDemelas <86729870+francescoDemelas@users.noreply.github.com>
2025-03-18 19:21:44 +0100

    Corrected SoftBundle

--- src/Bundle/SoftBundle.jl
+++ src/Bundle/SoftBundle.jl
@@ -1,5 +1,6 @@
 """
-SoftBundle structure.
+BatchedSoftBundle structure.
+It works as SoftBundle structure, but allows to handle multiple bundle execution in parallel in order to perform batching.
 This variant does not computes the search direction as solution of the dual master problem, but it needs to use another model (generally based on neural networks).
 """
 mutable struct SoftBundle <: AbstractSoftBundle
@@ -18,37 +19,36 @@ mutable struct SoftBundle <: AbstractSoftBundle
 	CSS::Int64
 	CNS::Int64
 	ϕ0::Any
-	li::Int
+	li::Int64
 	info::Dict
 	maxIt::Int
-	t::Real
+	t::Vector{Float32}
 end
 
 """
 Construct and initialize a SoftBundle structure.
-Considering to optimize (maximize) the function `ϕ` and the staring point `z`.
+Considering the functions contained in the vector `ϕs` and the staring points contained in the vector (of vectors) `z`.
 `ϕs` and `z` must have the same number of components.
 `lt` will be the factory of the model that we want consider for the prediction at the place of the Dual Master Problem.
 The Bundle will be initialized to perform `maxIt` iterations (by default `10`).
 """
-
-function initializeBundle(bt::SoftBundleFactory, ϕ::AbstractConcaveFunction, z::AbstractArray, lt::AbstractModelFactory, maxIt::Int = 10)
-	B = SoftBundle([], [], [], -1, [], [], [], Inf, [Inf], [Float32[]], lt, [], 0, 0, [], 1, Dict(), maxIt, 0.0)
-	#B.nn.h_representation = h_representation(nn)
-	obj, g = value_gradient(ϕ, z)
-	g = reshape(g, :)
+function initializeBundle(bt::SoftBundleFactory, ϕ::AbstractConcaveFunction, z::AbstractArray, lt, maxIt::Int = 10)
+	B = SoftBundle([], [], [], -1, [], [], [], Inf, [Inf], [Float32[]], lt, [], 0, 0, [], 1, Dict(), maxIt, [1.0])
+	tmp = 0
+	obj, g = value_gradient(ϕ, reshape(z, sizeInputSpace(ϕ)))
+	g = reshape(cpu(g), :)
 	B.s = 1
-	z = reshape(z, :)
-	B.z = device(zeros(Float32, length(g), B.maxIt + 1))
-	B.α = device(zeros(Float32, B.maxIt + 1))
-	B.G = hcat(g, device(zeros(Float32, length(g), B.maxIt)))
-
-	B.obj = vcat(obj, device(zeros(B.maxIt)))
-	B.objB = g' * g
+	B.z = zeros(Float32, length(z), B.maxIt + 1)
+	B.z[:, 1] = reshape(z, :)
+	B.α = zeros(Float32, 1, B.maxIt + 1)
+	B.G = zeros(Float32, length(g), B.maxIt + 1)
+	B.G[:, 1] = g
+	B.obj = zeros(Float32, 1, B.maxIt + 1)
+	B.obj[:, 1] = [obj]
+	B.objB = g'g
 	B.w = g
-	B.θ = zeros(B.maxIt + 1)
-	B.θ[1] = 1
-	B.ϕ0 = initializeϕ0(lt, ϕ)
+	B.θ = ones(1, 1)
+	B.li = 1
 	return B
 end
 
@@ -62,13 +62,12 @@ function reinitialize_Bundle!(B::SoftBundle, only_last::Bool = true)
 		B.li = 1
 		B.s = 1
 	end
-	B.obj = Zygote.bufferfrom(cpu(Float32.(B.obj)))
-	B.G = Zygote.bufferfrom(device(Float32.(hcat(B.G[:, 1], zeros(size(B.G, 1), B.maxIt)))))
-	B.z = Zygote.bufferfrom(device(Float32.(hcat(B.z[:, 1], zeros(size(B.z, 1), B.maxIt)))))
-	B.α = cpu(B.obj[:] .- B.obj[B.s]) .- cpu(sum(B.G[:, :] .* (B.z[:, :] .- B.z[:,B.s]); dims = 1))'
-	B.α = Zygote.bufferfrom(cpu(B.α))
-	B.θ = device(ones(B.li) ./ B.li)
-	B.w = device(B.w)
+	B.G = Zygote.bufferfrom(device(hcat([B.G[:, 1:B.li], zeros(size(B.G, 1), B.maxIt + 1 - B.li)]...)))
+	B.z = Zygote.bufferfrom(device(hcat([B.z[:, 1:B.li], zeros(size(B.z, 1), B.maxIt + 1 - B.li)]...)))
+	B.α = Zygote.bufferfrom(cpu(hcat(B.α[:, 1:B.li], zeros(size(B.α, 1), B.maxIt + 1 - B.li))))
+	B.obj = Zygote.bufferfrom(cpu(hcat(B.obj[:, 1:B.li], zeros(size(B.obj, 1), B.maxIt + 1 - B.li))))
+	B.θ = ones(1, 1)
+	B.w = device(zeros(size(B.z, 1)))
 end
 
 """
@@ -90,9 +89,9 @@ function bundle_execution(
 	metalearning = false,
 	unstable = false,
 	inference = false,
-	z_bar = Zygote.bufferfrom(Float32.(vcat(B.z[:, B.s]))),
+	z_bar = Zygote.bufferfrom(Float32.(vcat([B.z[:, B.s]]...))),
 	z_new = Zygote.bufferfrom(B.z[:, B.li]))
-	let xt, xγ, z_copy, LR_vec, Baseline, obj_new, obj_bar, g, t0, t1, times, maxIt, t, γs, θ
+	let xt, xγ, z_copy, LR_vec, Baseline, obj_new, obj_bar, g, t0, t1, times, maxIt, t, γs, θ, w
 		ignore_derivatives() do
 			times = Dict("init" => 0.0, "iters" => [], "model" => [], "distribution" => [], "features" => [], "stab_point" => [], "update_bundle" => [], "update_direction" => [], "update_point" => [], "lsp" => [])
 			maxIt = B.maxIt
@@ -101,10 +100,11 @@ function bundle_execution(
 		featG = function_features(B, B.lt)
 		ignore_derivatives() do
 			g = Zygote.bufferfrom(zeros(size(B.w)))
-			obj_new = Float32[cpu(B.obj[B.li])]
-			obj_bar = Float32[cpu(B.obj[B.s])]
-			γs = [device(zeros(1, it)) for it in 1:maxIt]
-			θ = [device(zeros(1, it)) for it in 1:maxIt]
+			obj_new = Zygote.bufferfrom(cpu(B.obj[B.li.*ones(Int64, 1)]))
+			obj_bar = Zygote.bufferfrom(cpu(B.obj[B.s.*ones(Int64, 1)]))
+			w = Zygote.bufferfrom([device(zeros(length(B.w))) for it in 1:maxIt])
+			γs = Zygote.bufferfrom([device(zeros(1, it)) for it in 1:maxIt])
+			θ = Zygote.bufferfrom([device(zeros(1, it)) for it in 1:maxIt])
 			B.objB = 0
 			times["init"] = time() - t0
 		end
@@ -129,14 +129,14 @@ function bundle_execution(
 			ignore_derivatives() do
 				t1 = time()
 			end
+			t, γs[it] = m(xt, xγ)
 
+			ignore_derivatives() do
+				B.t = reshape(t, :)
+			end
 
-			res = m(xt, xγ)
-
-			t = copy(res[1])
-			B.t = sum(t)
-			γs[it] = copy(reshape(res[2], size(γs[it])))  # Vérifie si res est un tuple
 			min_idx = Int64(max(1, minimum(B.li) - max_inst))
+
 			ignore_derivatives() do
 				append!(times["model"], time() - t1)
 			end
@@ -156,9 +156,10 @@ function bundle_execution(
 				t1 = time()
 			end
 
-			B.w = vcat(B.G[:, min_idx:B.li] * θ[it][1, :])
+			w[it] = B.G[:, min_idx:B.li] * θ[it][1, :]
 
 			ignore_derivatives() do
+				B.w = w[it]
 				append!(times["update_direction"], time() - t1)
 			end
 
@@ -166,7 +167,7 @@ function bundle_execution(
 				t1 = time()
 			end
 
-			z_new[:] = z_bar[:] + vcat(B.t .* B.w[:])
+			z_new[:] = z_bar[:] .+ sum(t) .* w[it]
 
 			ignore_derivatives() do
 				append!(times["update_point"], time() - t1)
@@ -178,33 +179,32 @@ function bundle_execution(
 
 			v, g_tmp = value_gradient(ϕ, z_new[:])
 			g[:] = cpu(g_tmp)
-			obj_new = v
+			obj_new[1] = v
 
 			ignore_derivatives() do
 				append!(times["lsp"], time() - t1)
 			end
 
 			B.li += 1
+
 			ignore_derivatives() do
 				t1 = time()
 			end
+
 			if unstable
 				z_bar = z_new
 				obj_bar = obj_new
 				B.s = B.li .* ones(Int64, length(B.s))
 			else
 				if !soft_updates
-
-					z_bar[:] = (obj_new > obj_bar ? (z_new[:]) : (z_bar[:]))
-					obj_bar = (obj_new > obj_bar ? obj_new[:] : obj_bar[:])
+					z_bar[:] = obj_new[:] > obj_bar[:] ? (z_new[:]) : (z_bar[:])
+					obj_bar[:] = (obj_new[:] > obj_bar[:] ? obj_new[:] : obj_bar[:])
 				else
-
-					sm = softmax([obj_new, obj_bar])
-					obj_bar = sm' * [obj_new, obj_bar]
+					sm = softmax([obj_new[1], obj_bar[1]])
+					obj_bar[1] = sm' * [obj_new[1], obj_bar[1]]
 					z_bar[:] = device(sm' * cpu.([z_new[:], z_bar[:]]))
-
 				end
-				B.s = obj_new > obj_bar ? B.li : B.s
+				B.s = sum(obj_new[:] > obj_bar[:] ? B.li : B.s)
 			end
 			ignore_derivatives() do
 				append!(times["stab_point"], time() - t1)
@@ -215,10 +215,9 @@ function bundle_execution(
 			end
 			B.G[:, B.li] = device(g[:])
 			B.z[:, B.li] = z_new[:]
-			B.obj[B.li]  = obj_new
+			B.obj[:, B.li] = obj_new[:]
 
-			B.α[1:B.li] = (B.obj[1:B.li] .- obj_bar) .- cpu(sum(B.G[:, 1:B.li] .* (B.z[:, 1:B.li] .- z_bar[:]); dims = 1))'
-
+			B.α[1, :] = (B.obj[1, :] .- obj_bar[1, :]) .- cpu(sum(B.G[:, :] .* (B.z[:, :] .- z_bar[:]); dims = 1))'
 
 			ignore_derivatives() do
 				append!(times["update_bundle"], time() - t1)
@@ -226,8 +225,9 @@ function bundle_execution(
 			end
 		end
 
+
 		if inference
-			return ϕ[iϕ](reshape(z_bar, sizeInputSpace(ϕ))), times
+			return ϕ(reshape(z_bar, sizeInputSpace(ϕ))), times
 		else
 			vγ = (γ > 0 ? γ * mean([γ^(B.maxIt + 1 - i) for i in (B.maxIt+1):-1:1] .* [ϕ(z) for z in eachcol(B.z[:, 1:B.maxIt+1])]) : 0)
 			vλ = (1 - λ) * ϕ(reshape(z_bar[:], sizeInputSpace(ϕ))) + (λ) * ϕ(reshape(z_new[:], sizeInputSpace(ϕ)))
