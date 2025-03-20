2f0c9545e7509450cd98829594f748aac150bc55
francescoDemelas <86729870+francescoDemelas@users.noreply.github.com>
2025-03-20 18:09:53 +0100

    Added Comments to Deviations

--- src/Models/Deviations.jl
+++ src/Models/Deviations.jl
@@ -1,20 +1,34 @@
-
+"""
+Abstact type for deviations used in model that predicts only `t`.
+"""
 abstract type AbstractDeviation
 end
 
+"""
+Additive deviations consider `t_new = t_old + dev` where `dev` is predicted by the model.
+"""
 struct AdditiveDeviation <: AbstractDeviation end
 
-struct MultiplicativeDeviation <: AbstractDeviation end
-
-struct NothingDeviation <: AbstractDeviation end
-
 (dt::AdditiveDeviation)(t,dev) = t .+ dev
-(dt::MultiplicativeDeviation)(t,dev) = t .* dev
-(dt::NothingDeviation)(_,dev) = dev
-
 
 Flux.@layer AdditiveDeviation
-	
+
+
+"""
+Multiplicative deviations consider `t_new = t_old * dev` where `dev` is predicted by the model.
+"""
+struct MultiplicativeDeviation <: AbstractDeviation end
+
+(dt::MultiplicativeDeviation)(t,dev) = t .* dev
+
 Flux.@layer MultiplicativeDeviation
+
+
+"""
+Nothing deviations consider `t_new = dev` where `dev` is predicted by the model.
+"""
+struct NothingDeviation <: AbstractDeviation end
+
+(dt::NothingDeviation)(_,dev) = dev
 	
 Flux.@layer NothingDeviation
